03cd43e42b740d95c33c39173a573cb40abc65f7
zhaoqing <qingzhao566@gmail.com>
2025-01-19 00:13:16 -0800

    Android Project Created

--- app/src/main/java/com/example/numad25sp_qingzhao/MainActivity.java
+++ app/src/main/java/com/example/numad25sp_qingzhao/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.numad25sp_qingzhao;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +25,20 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        TextView t = findViewById(R.id.textView);
+
+        Button b = findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Toast message
+                Toast toast = new Toast(MainActivity.this);
+                toast.setText("Qing Zhao: zhao.qing@northeastern.edu");
+                toast.setDuration(Toast.LENGTH_LONG);
+                toast.show();
+            }
         });
     }
 }
